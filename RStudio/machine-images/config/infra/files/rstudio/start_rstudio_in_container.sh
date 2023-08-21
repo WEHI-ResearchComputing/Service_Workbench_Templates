19433eb1672ff93bfefb6b2ae22dfc69fb409a3f
Julian Kelabora <julian@kelabora.com>
2023-08-21 16:02:04 +1000

    switch to using docker start instead of docker run to preserve filesystem on host EC2 start/stop

--- RStudio/machine-images/config/infra/files/rstudio/start_rstudio_in_container.sh
+++ RStudio/machine-images/config/infra/files/rstudio/start_rstudio_in_container.sh
@@ -2,13 +2,17 @@
 
 set -ex
 
-pword=$(cat /root/secret.txt)
-echo $pword
-/usr/bin/docker run \
-  --privileged \
-  --rm \
-  -e PASSWORD=$pword \
-  -p 8787:8787 \
-  --mount type=bind,source=/usr/local/,target=/host \
-  010045907075.dkr.ecr.ap-southeast-2.amazonaws.com/rocker_tidyverse_4.3.0:1 \
-  &
+# 1) create container locally with:
+# pword=$(cat /root/secret.txt)
+# echo $pword
+# /usr/bin/docker run \
+#   --privileged \
+#   -e PASSWORD=$pword \
+#   -p 8787:8787 \
+#   --mount type=bind,source=/usr/local/,target=/host \
+#   010045907075.dkr.ecr.ap-southeast-2.amazonaws.com/rocker_tidyverse_4.3.0:1
+
+# 2) get local container name with: docker container ls -a
+
+# 3) start the local container by name:
+/usr/bin/docker start reverent_cori
