8eabc055729c067822d0e21f8daa9976543615d5
Julian Kelabora <julian@kelabora.com>
2023-08-22 13:37:48 +1000

    switch over to re-using a local docker container on the EC2 host so that filesystem changes are preserved across workspace start/stop cycles

--- RStudio/machine-images/config/infra/files/docker/mount_s3_using_rclone.sh
+++ RStudio/machine-images/config/infra/files/docker/mount_s3_using_rclone.sh
@@ -9,7 +9,6 @@ rclone mount \
   --uid 1000 \
   --gid 1000 \
   --cache-dir /root/rclone/vfs-cache-dir/ \
-  --no-modtime \
   --vfs-cache-mode writes \
   ASPREE-combined:904145425705-prod-syd-sw-studydata/studies/Organization/ASPREE-combined \
   /home/rstudio/studies
--- RStudio/machine-images/config/infra/files/rstudio/start_rstudio_in_container.sh
+++ RStudio/machine-images/config/infra/files/rstudio/start_rstudio_in_container.sh
@@ -2,17 +2,18 @@
 
 set -ex
 
-# 1) create container locally with:
-# pword=$(cat /root/secret.txt)
-# echo $pword
+# 1) grab the correct published container:
+# aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 010045907075.dkr.ecr.ap-southeast-2.amazonaws.com
+# /usr/bin/docker pull 010045907075.dkr.ecr.ap-southeast-2.amazonaws.com/rocker_tidyverse_4.3.0:2
+
+# 2) create container locally with:
 # /usr/bin/docker run \
 #   --privileged \
-#   -e PASSWORD=$pword \
 #   -p 8787:8787 \
-#   --mount type=bind,source=/usr/local/,target=/host \
-#   010045907075.dkr.ecr.ap-southeast-2.amazonaws.com/rocker_tidyverse_4.3.0:1
+#   010045907075.dkr.ecr.ap-southeast-2.amazonaws.com/rocker_tidyverse_4.3.0:2
 
-# 2) get local container name with: docker container ls -a
+# 3) get local container name with:
+# docker container ls -a
 
-# 3) start the local container by name:
-/usr/bin/docker start reverent_cori
+# 4) start the local container by name:
+/usr/bin/docker start pensive_wilson
