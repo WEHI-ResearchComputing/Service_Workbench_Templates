33e33dc0d233aa226b39d5918bd0f64a4ef04fa8
Julian Kelabora <julian@kelabora.com>
2024-04-19 11:06:30 +1000

    bump the docker image used in the ec2 instance for workspaces

--- RStudio/machine-images/config/infra/files/rstudio/start_rstudio_in_container.sh
+++ RStudio/machine-images/config/infra/files/rstudio/start_rstudio_in_container.sh
@@ -4,7 +4,7 @@ set -ex
 
 # 1) grab the correct published container:
 # aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 010045907075.dkr.ecr.ap-southeast-2.amazonaws.com
-# /usr/bin/docker pull 010045907075.dkr.ecr.ap-southeast-2.amazonaws.com/analysisenv:1
+# /usr/bin/docker pull 010045907075.dkr.ecr.ap-southeast-2.amazonaws.com/analysisenv:2
 
 # 2) create container locally with:
 # https://docs.docker.com/engine/reference/commandline/create/
@@ -12,8 +12,8 @@ set -ex
 #   --privileged \
 #   -p 8787:8787 \
 #   -i -t \
-#   --name analysisenv_1 \
-#   010045907075.dkr.ecr.ap-southeast-2.amazonaws.com/analysisenv:1
+#   --name analysisenv_2 \
+#   010045907075.dkr.ecr.ap-southeast-2.amazonaws.com/analysisenv:2
 
 # 3) start the local container by name:
-/usr/bin/docker start analysisenv_1
+/usr/bin/docker start analysisenv_2
